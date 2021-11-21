a04f67c9f94cf5b7e7d1f7b0ee651301c790e657
Thomaz Peres <thomaz.peres21@hotmail.com>
2021-11-20 23:50:40 -0300

    Adicionando procura por monstro

---
+++ Commands/BattleCommands.cs
@@ -0,0 +1,30 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using FirstBotDiscord.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstBotDiscord.Commands
+{    
+    public class BattleCommands : BaseCommandModule
+    {
+        private readonly BattleService _battleService;
+        public BattleCommands(BattleService battleService)
+        {
+            _battleService = battleService;
+        }
+
+        [Command("SearchEnemy")]
+        [Aliases("se")]
+        [Description("Procura inimigo para batalhar")]
+        public async Task SearchEnemyCommand(CommandContext ctx, int monsterLevel)
+        {
+            await ctx.TriggerTypingAsync();
+
+            await _battleService.SearchEnemy(monsterLevel);
+        }
+    }
+}
---
+++ Services/BattleService.cs
@@ -0,0 +1,24 @@
+﻿using DSharpPlus.Entities;
+using FirstBotDiscord.Database;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstBotDiscord.Services
+{
+    public class BattleService
+    {
+        private readonly DataContext _dataContext;
+
+        public BattleService(DataContext dataContext) =>
+            _dataContext = dataContext;
+
+        public async Task SearchEnemy(int monsterLevel)
+        {
+            var monster = await _dataContext.CollectionMonsters.Find(x => x.Level == monsterLevel).FirstOrDefaultAsync();
+        }
+    }
+}
