f1f8d06bf219e15bb78d995751c316f4f211777e
Thomaz <thomaz.peres21@hotmail.com>
2022-01-30 22:02:47 -0300

    Começando a mexer nos level

--- Entities/Rpg/Player/LevelEntity.cs
+++ Entities/Rpg/Player/LevelEntity.cs
@@ -3,7 +3,29 @@ namespace FirstBotDiscord.Entities.Rpg.Player
     public class LevelEntity
     {
         public int Level { get; set; } = 1;
-        public double CurrentExperience { get; set; } = 0.00;
-        public double ExperienceForNextLevel { get; set; } = 100.00;
+        public double CurrentExperience { get; set; }
+        public double ExperienceForNextLevel { get; set; }
+
+        public void AddExperience(double xp)
+        {
+            this.CurrentExperience += xp;
+
+            do
+            {
+                this.LevelUp();
+            }
+            while(this.CurrentExperience >= this.ExperienceForNextLevel);
+        }
+
+        public void LevelUp()
+        {
+            this.Level++;
+            ExperienceNextLevel(this.Level);
+        }
+
+        public void ExperienceNextLevel(int level)
+        {
+
+        }
     }
 }
