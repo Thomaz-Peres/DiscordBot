7607db4855b0d442025249799508b8bb9e3f7ec8
Thomaz <thomaz.peres21@hotmail.com>
2022-02-01 22:50:24 -0300

    pesquisando e pensando formulas para defesa fisica (mexi pouco hoje)

--- Entities/Rpg/RpgMonsters/MonsterAttacks.cs
+++ Entities/Rpg/RpgMonsters/MonsterAttacks.cs
@@ -15,10 +15,23 @@ namespace FirstBotDiscord.Entities.Rpg.RpgMonsters
         public MonsterAttacks (DataContext dataContext) =>
             _dataContext = dataContext;
 
-        public int BasicAttack(CommandContext ctx, BaseMonstersEntity monster, PlayerEntity playerCharacter)
+        public int AttackBasicMonsterBasic(CommandContext ctx, BaseMonstersEntity monster, PlayerEntity playerCharacter)
         {
+            var baseAtaque = new Random().Next(5, 15) + new Random().Next((int)monster.PhysicalAttack.CurrentOrMinValuePoints, (int)monster.PhysicalAttack.MaxValuePoints);
 
 
+            // formula sobre defesa a.atk / (1 + b.def / 100)
+            /* O que essa formula significa? Significa que um personagem com 0 de DEF irá levar dano exatamente igual ao atk do agressor(atk / 1).Cada ponto de def além do primeiro irá aumentar o divisor em 0,01.Resultado:
+
+            Um personagem com 0 DEF recebe 100 % do dano de um ataque.
+            Um personagem com 100 DEF recebe 50 % do dano de um ataque.
+            Um personagem com 200 DEF recebe 33,3 % do dano de um ataque.
+            Um personagem com 300 DEF recebe 25 % do dano de um ataque.
+            Um personagem com 400 DEF recebe 20 % do dano de um ataque. */
+
+            var armor = new Random().Next((int)playerCharacter.MyCharacter.Armor.CurrentOrMinValuePoints, (int)playerCharacter.MyCharacter.Armor.MaxValuePoints);
+
+            playerCharacter.MyCharacter.LifePoints.CurrentOrMinValuePoints -= baseAtaque / (1 + armor / 100);
 
 
             return 0;
